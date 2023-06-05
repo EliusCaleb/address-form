b9056d26d908487307531795ec4b99a1261d0e8d
ELIUS WANYORO <109586789+EliusCaleb@users.noreply.github.com>
2023-06-05 21:07:28 +0300

    ggetting input from form Data

--- index.js
+++ index.js
@@ -50,6 +50,14 @@ function eventListener(){
 
     // close add item modal
     closeBtn.addEventListener('click', UI.closeModal);
+    modalBtns.addEventListener('click', (event) => {
+        event.preventDefault();
+        if(event.target.id == "save-btn"){
+            console.log("qwerty")
+            let isFormValid = getFormData()
+        }
+
+    })
 }
 
 
@@ -66,4 +74,82 @@ function loadJSON(){
         })
         countryList.innerHTML = html
     })
+}
+
+// get form data
+function getFormData(){
+    let inputValidStatus = [];
+     //console.log(form.addr_ing_name.value, form.first_name.value, form.last_name.value, form.email.value, form.phone.value, form.street_addr.value, form.postal_code.value, form.city.value, form.country.value, form.labels.value);
+
+    if(!strRegex.test(form.addr_ing_name.value) || form.addr_ing_name.value.trim().length == 0){
+        addErrMsg(form.addr_ing_name);
+        inputValidStatus[0] = false;
+    } else {
+        addrName = form.addr_ing_name.value;
+        inputValidStatus[0] = true;
+    }
+
+    if(!strRegex.test(form.first_name.value) || form.first_name.value.trim().length == 0){
+        addErrMsg(form.first_name);
+        inputValidStatus[1] = false;
+    } else {
+        firstName = form.first_name.value;
+        inputValidStatus[1] = true;
+    }
+
+    if(!strRegex.test(form.last_name.value) || form.last_name.value.trim().length == 0){
+        addErrMsg(form.last_name);
+        inputValidStatus[2] = false;
+    } else {
+        lastName = form.last_name.value;
+        inputValidStatus[2] = true;
+    }
+
+    if(!emailRegex.test(form.email.value)){
+        addErrMsg(form.email);
+        inputValidStatus[3] = false;
+    } else {
+        email = form.email.value;
+        inputValidStatus[3] = true;
+    }
+
+    if(!phoneRegex.test(form.phone.value)){
+        addErrMsg(form.phone);
+        inputValidStatus[4] = false;
+    } else {
+        phone = form.phone.value;
+        inputValidStatus[4] = true;
+    }
+
+    if(!(form.street_addr.value.trim().length > 0)){
+        addErrMsg(form.street_addr);
+        inputValidStatus[5] = false;
+    } else {
+        streetAddr = form.street_addr.value;
+        inputValidStatus[5] = true;
+    }
+
+    if(!digitRegex.test(form.postal_code.value)){
+        addErrMsg(form.postal_code);
+        inputValidStatus[6] = false;
+    } else {
+        postCode = form.postal_code.value;
+        inputValidStatus[6] = true;
+    }
+
+    if(!strRegex.test(form.city.value) || form.city.value.trim().length == 0){
+        addErrMsg(form.city);
+        inputValidStatus[7] = false;
+    } else {
+        city = form.city.value;
+        inputValidStatus[7] = true;
+    }
+    country = form.country.value;
+    labels = form.labels.value;
+    return inputValidStatus.includes(false) ? false : true;
+}
+
+
+function addErrMsg(inputBox){
+    inputBox.classList.add('errorMsg');
 }
